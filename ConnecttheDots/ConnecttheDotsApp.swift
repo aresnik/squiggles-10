e12b40594c78143ea5971da016b6616fcbfd7b55
Alex Resnik <alex@alexanderresnik.com>
2023-02-27 10:17:06 -0500

    Initial Commit

---
+++ ConnecttheDots/ConnecttheDotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnecttheDotsApp.swift
+//  ConnecttheDots
+//
+//  Created by Alex Resnik on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnecttheDotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
