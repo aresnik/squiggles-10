17965a832e5de78da92941c36c2e154265d32556
Alex Resnik <alex@alexanderresnik.com>
2023-03-28 16:56:10 -0400

    Update

--- Connect/Board.swift
+++ Connect/Board.swift
@@ -19,9 +19,10 @@ struct Board: View {
             Color.black
                 .ignoresSafeArea()
             grid
-            middle
+//            middle
+            drawLines
             overlay
-        }.onAppear(perform: viewModel.start)
+        }.onAppear(perform: viewModel.randomize)
     }
 }
 
@@ -33,14 +34,13 @@ extension Board {
                 HStack(spacing: 0.0) {
                     ForEach(0..<10) { x in
                         ZStack {
-                            let i = x + y*10
                             Rectangle()
                                 .stroke(.white)
                                 .scaledToFit()
+                            let dot = viewModel.dots.first { $0.x == x && $0.y == y }
                             Circle()
-                                .fill(viewModel.end[i])
+                                .fill(dot?.color ?? .clear)
                                 .frame(width: screenWidth/16)
-                            
                         }
                     }
                 }
@@ -51,57 +51,94 @@ extension Board {
 }
 extension Board {
     private var middle: some View {
-        VStack(spacing: 0.0) {
-            Spacer()
-            ForEach(0..<10) { y in
-                HStack(spacing: 0.0) {
-                    ForEach(0..<10) { x in
-                        ZStack {
-                            let i = x + y*10
-                            Rectangle()
-                                .stroke(.clear)
-                                .frame(width: screenWidth/10, height: screenWidth/10)
-                            Rectangle()
-                                .fill(viewModel.middle[i])
-                                .frame(width: screenWidth/30, height: screenWidth/30)
-                        }
-                    }
+        ForEach(viewModel.flows, id: \.color) { flow in
+            Path { path in
+                path.move(to: position(at: flow.middles.first ?? 0))
+                for i in 0..<flow.middles.count {
+                    path.addLine(to: position(at: flow.middles[i]))
                 }
             }
-            Spacer()
+            .stroke(flow.color, lineWidth: 8)
+            .offset(CGSize(width: 20, height: 155))
         }
+    }
+    
+    // Convert index to position
+    func position(at index: Int) -> CGPoint {
+        let row = index / 10
+        let col = index % 10
+        return CGPoint(x: CGFloat(col) * screenWidth/10, y: CGFloat(row) * screenWidth/10)
     }
 }
 
 extension Board {
-    var overlay: some View {
-        VStack(spacing: 0.0) {
-            Spacer()
-            ForEach(0..<10) { _ in
-                HStack(spacing: 0.0) {
-                    ForEach(0..<10) { _ in
-                        Rectangle()
-                            .fill(.clear)
-                            .contentShape(Rectangle())
-                            .gesture(DragGesture(coordinateSpace: .named("overlay"))
-                                .onChanged { value in
-                                    let x = Int(value.location.x/(screenWidth/10))
-                                    let y = Int(value.location.y/(screenWidth/10))
-                                    var i = x + y*10
-                                    if i < 0  { i = 0  }
-                                    if i > 99 { i = 99 }
-                                    viewModel.move(i: i)
-                                }
-                            )
-                    }
-                }
+    private var drawLines: some View {
+        ForEach(viewModel.lines, id: \.color) { line in
+            Path { path in
+                path.move(to: CGPoint(x: line.start.x, y: line.start.y))
+                path.addLine(to: CGPoint(x: line.end.x, y: line.end.y))
             }
-            Spacer()
+            .stroke(line.color, lineWidth: 8)
+            .offset(CGSize(width: 20, height: 155))
         }
-        .coordinateSpace(name: "overlay")
-        .frame(width: screenWidth, height: screenWidth)
     }
 }
+
+
+extension Board {
+    var overlay: some View {
+        VStack(spacing: 0.0) {
+            Color.clear
+                .background(grid)
+                .gesture(
+                    DragGesture(minimumDistance: 0.0, coordinateSpace: .named("overlay"))
+                        .onChanged { value in
+                            // Find the dot that was tapped
+                            let x = Int(value.location.x/(screenWidth/10))
+                            let y = Int(value.location.y/(screenWidth/10))
+                            guard let dot = viewModel.dots.first(where: { $0.x == x && $0.y == y }) else {
+                                return
+                            }
+
+                            // Check if there is already a line starting or ending at this dot
+                            if viewModel.lines.contains(where: { $0.start == dot || $0.end == dot }) {
+                                return
+                            }
+                            
+                            // Create a new line starting at this dot
+                            viewModel.lines.append(Model.Line(color: dot.color, start: dot, end: dot))
+                        }
+                        .onChanged { value in
+                            // Find the dot that the touch is currently over
+                            let x = Int(value.location.x/(screenWidth/10))
+                            let y = Int(value.location.y/(screenWidth/10))
+                            guard let dot = viewModel.dots.first(where: { $0.x == x && $0.y == y }) else {
+                                return
+                            }
+                            
+                            // Find the line that is currently being drawn
+                            guard var line = viewModel.lines.last else {
+                                return
+                            }
+                            
+                            // Update the end point of the line to the current dot
+                            line.end = dot
+                            
+                            // Update the lines array with the new line
+                            viewModel.lines[viewModel.lines.count - 1] = line
+                        }
+                        .onEnded { value in
+                            // Find the dot that the touch ended on
+                            let x = Int(value.location.x/(screenWidth/10))
+                            let y = Int(value.location.y/(screenWidth/10))
+                            _ = viewModel.dots.first(where: { $0.x == x && $0.y == y })
+                        }
+                )
+                .coordinateSpace(name: "overlay")
+        }
+    }
+}
+
 struct Board_Previews: PreviewProvider {
     static var previews: some View {
         Board()
--- Connect/Model.swift
+++ Connect/Model.swift
@@ -9,31 +9,93 @@ import Foundation
 import SwiftUI
 
 final class Model: ObservableObject {
-
-    @Published var end: [Color] = Array(repeating: .clear, count: 100)
-    @Published var middle: [Color] = Array(repeating: .clear, count: 100)
-    private var flowColor: Color = .clear
     
-    func start() {
-        end[0] = .red;     end[9] = .red
-        end[10] = .blue;   end[19] = .blue
-        end[20] = .green;  end[29] = .green
-        end[30] = .orange; end[39] = .orange
-        end[40] = .yellow; end[49] = .yellow
-        end[50] = .gray;   end[59] = .gray
-        end[60] = .purple; end[69] = .purple
-        end[70] = .brown;  end[79] = .brown
-        end[80] = .cyan;   end[89] = .cyan
-        end[90] = .indigo; end[99] = .indigo
+    @Published var lines: [Line] = []
+    @Published var dots: [Dot] = []
+    private var color: [Color] = [
+        .red, .blue, .green, .orange, .yellow, .gray, .purple, .brown, .cyan, .indigo ].shuffled()
+    
+    struct Flow {
+        var color: Color
+        var middles: [Int]
+    }
+    
+    struct Dot: Equatable {
+        var color: Color
+        var x: Int
+        var y: Int
+    }
+    
+    struct Line: Equatable {
+        var color: Color
+        var start: Dot
+        var end: Dot
+    }
+    
+    // Initial flows
+    @Published var flows: [Flow] = [
+        Flow(color: .red,    middles: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]),
+        Flow(color: .blue,   middles: [1, 11, 21, 31, 41, 51, 61, 71, 81]),
+        Flow(color: .green,  middles: [2, 12, 22, 32, 42, 52, 62, 72, 82]),
+        Flow(color: .orange, middles: [3, 13, 23, 33, 43, 53, 63, 73, 83]),
+        Flow(color: .yellow, middles: [4, 14, 24, 34, 44, 54, 64, 74, 84]),
+        Flow(color: .gray  , middles: [5, 15, 25, 35, 45, 55, 65, 75, 85]),
+        Flow(color: .purple, middles: [6, 16, 26, 36, 46, 56, 66, 76, 86]),
+        Flow(color: .brown,  middles: [7, 17, 27, 37, 47, 57, 67, 77, 87]),
+        Flow(color: .cyan,   middles: [8, 18, 28, 38, 48, 58, 68, 78, 88]),
+        Flow(color: .indigo, middles: [9, 19, 29, 39, 49, 59, 69, 79, 89]) ]
+    
+    func randomizecolors() {
+        for i in 0..<flows.count {
+            flows[i].color = color[i]
+        }
+    }
+    
+    func drawDots() {
+        for i in 0..<flows.count {
+            dots.append(Dot(color: color[i], x: (flows[i].middles.first ?? 0) % 10,
+                                             y: (flows[i].middles.first ?? 0) / 10))
+            dots.append(Dot(color: color[i], x: (flows[i].middles.last ?? 0) % 10,
+                                             y: (flows[i].middles.last ?? 0) / 10))
+        }
     }
     
     func randomize() {
-
+        for _ in 0..<100 {
+        let rnd: Int = Int.random(in: 0..<flows.count)
+            for i in 0..<flows.count {
+                if flows[rnd].middles.count > 3 {
+                    if isNeighbor(end1: flows[rnd].middles.first ?? 0, end2: flows[i].middles.first ?? 0) {
+                        flows[i].middles.insert(flows[rnd].middles.first ?? 0, at: 0)
+                        flows[rnd].middles.removeFirst()
+                    }
+                    if isNeighbor(end1: flows[rnd].middles.last ?? 9, end2: flows[i].middles.last ?? 9) {
+                        flows[i].middles.append(flows[rnd].middles.last ?? 9)
+                        flows[rnd].middles.removeLast()
+                    }
+                }
+            }
+        }
+        randomizecolors()
+        drawDots()
     }
     
     func move(i: Int) {
-        if end[i] != .clear { flowColor = end[i] }
-        middle[i] = flowColor
+
+    }
+    
+    func isNeighbor(end1: Int, end2: Int) -> Bool {
+        return ( abs(end1 - end2) == 1 || abs(end1 - end2) == 10 ) &&
+        !(end1 == 0  && end2 == 19) && !(end2 ==  0 && end1 == 19) &&
+        !(end1 == 10 && end2 ==  9) && !(end2 == 10 && end1 ==  9) &&
+        !(end1 == 20 && end2 == 19) && !(end2 == 20 && end1 == 19) &&
+        !(end1 == 30 && end2 == 29) && !(end2 == 30 && end1 == 29) &&
+        !(end1 == 40 && end2 == 39) && !(end2 == 40 && end1 == 39) &&
+        !(end1 == 50 && end2 == 49) && !(end2 == 50 && end1 == 49) &&
+        !(end1 == 60 && end2 == 59) && !(end2 == 60 && end1 == 59) &&
+        !(end1 == 70 && end2 == 69) && !(end2 == 70 && end1 == 69) &&
+        !(end1 == 80 && end2 == 79) && !(end2 == 80 && end1 == 79) &&
+        !(end1 == 90 && end2 == 89) && !(end2 == 90 && end1 == 89)
     }
 }
 
